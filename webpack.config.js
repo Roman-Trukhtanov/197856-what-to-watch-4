c215b3d22e8ed7d2d70b5d489c5fca28d70690ee
Roman Trukhtanov <romantrukhtanov@gmail.com>
2020-07-14 17:07:45 +0500

    changed: components extension (js to jsx)

--- webpack.config.js
+++ webpack.config.js
@@ -17,14 +17,14 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        resolve: {
-          extensions: [`.js`, `.jsx`],
-        },
         use: {
           loader: `babel-loader`,
         }
       }
     ]
+  },
+  resolve: {
+    extensions: [`.js`, `.jsx`]
   },
   devtool: `source-map`,
 };
